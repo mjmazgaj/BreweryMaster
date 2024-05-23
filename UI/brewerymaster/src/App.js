57cf63af2b8e863d340a553fdaeb6c0ca6618ea0
Mazgaj <>
2024-05-23 20:10:36 +0200

    Add routing

--- UI/brewerymaster/src/App.js
+++ UI/brewerymaster/src/App.js
@@ -1,10 +1,19 @@
 import Navigation from './Components/Basic/Navigation';
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <>
       <Navigation />
+      <Router>
+        <div className="App">
+          <Routes>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
