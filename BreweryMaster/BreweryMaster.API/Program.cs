56075f9d70f3fd4c1d64e9252e82ce2faf7edb9b
Mazgaj <>
2024-05-28 14:45:20 +0200

    Backend | Add User Controller with Get info and logout endpoints

--- BreweryMaster/BreweryMaster.API/Program.cs
+++ BreweryMaster/BreweryMaster.API/Program.cs
@@ -1,17 +1,28 @@
 using BreweryMaster.API.Models.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
 using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BreweryMaster")));
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BreweryMaster")));
+builder.Services.AddDbContext<WorkDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BreweryMaster")));
 
-builder.Services.AddAuthorization();
-builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddAuthentication()
+                .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddAuthorizationBuilder();
+
+builder.Services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddApiEndpoints();
 
 builder.Services.AddCors(options =>
 {
@@ -35,20 +46,6 @@ var app = builder.Build();
 app.MapIdentityApi<ApplicationUser>();
 
 app.UseCors("ReactPolicy");
-
-app.MapPost("/logout", async (SignInManager<ApplicationUser> signInManager) =>
-{
-
-    await signInManager.SignOutAsync();
-    return Results.Ok();
-
-}).RequireAuthorization();
-
-app.MapGet("/pingauth", (ClaimsPrincipal user) =>
-{
-    var email = user.FindFirstValue(ClaimTypes.Email); // get the user's email from the claim
-    return Results.Json(new { Email = email }); ; // return the email as a plain text response
-}).RequireAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
