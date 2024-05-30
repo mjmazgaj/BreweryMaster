e5c2ccc2b2b553500d0806af025a20e55ffadd48
Mazgaj <>
2024-05-30 23:39:06 +0200

    Finito

--- UI/brewerymaster/src/Components/Basic/Home.jsx
+++ UI/brewerymaster/src/Components/Basic/Home.jsx
@@ -2,7 +2,17 @@ import './../../App.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <h1>System do zarzadzania browarem</h1>
+      <h3>Zakres:</h3>
+      <h4>Tablica kanbanowa</h4>
+      <h4>Logowanie</h4>
+      <h4>Rejestrowanie</h4>
+      <h4>Zarzadzanie taskami</h4>
+      <h4>Zarzadzanie klientami</h4>
+      <h4>Zarzadzanie urzytkownikami</h4>
+      <h4>Zarzadzanie adresami</h4>
+    </>
   );
 }
 
