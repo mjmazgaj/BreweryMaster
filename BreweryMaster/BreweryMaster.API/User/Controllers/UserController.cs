bdeb528b2d5ee39cba7828c44c0e0d1819925203
Mazgaj <>
2024-11-25 22:43:57 +0100

    Add custom registration endpoint

--- BreweryMaster/BreweryMaster.API/User/Controllers/UserController.cs
+++ BreweryMaster/BreweryMaster.API/User/Controllers/UserController.cs
@@ -1,24 +1,51 @@
 ﻿using BreweryMaster.API.User.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace BreweryMaster.API.User.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class UserController : ControllerBase
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UserController(SignInManager<ApplicationUser> signInManager)
+        public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
-        [HttpGet("info")]
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(RegisterUserRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (request.Password != request.ConfirmPassword)
+                return BadRequest(new { message = "Passwords do not match." });
+
+            var user = new ApplicationUser
+            {
+                UserName = request.Email,
+                Email = request.Email,
+                TestBool = request.TestBool
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            return Ok(new { message = "User registered successfully." });
+        }
+
+        [HttpGet]
+        [Route("info")]
         public IActionResult GetInfo()
         {
             var user = HttpContext.User;
@@ -38,7 +65,8 @@ namespace BreweryMaster.API.User.Controllers
             return Unauthorized();
         }
 
-        [HttpPost("logout")]
+        [HttpPost]
+        [Route("logout")]
         public ActionResult LogOff()
         {
             _signInManager.SignOutAsync();
---
+++ BreweryMaster/BreweryMaster.API/User/Models/RegisterUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace BreweryMaster.API.User.Models
+{
+    public class RegisterUserRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public bool TestBool { get; set; }
+    }
+}
