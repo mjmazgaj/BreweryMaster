5e39b08e179f917809053714b2236b5f68a7a6c4
Mazgaj <>
2025-01-01 13:38:07 +0100

    Update FermentingIngredientsOrder to desgin

--- UI/brewerymaster/src/Components/Info/Ingredients/FermentingIngredients/FermentingIngredientsComponents/FermentingIngredientsOrder.jsx
+++ UI/brewerymaster/src/Components/Info/Ingredients/FermentingIngredients/FermentingIngredientsComponents/FermentingIngredientsOrder.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../../../info.css";
 
 import DynamicTable from "../../../../Shared/TableComponents/DynamicTable";
-import ModalItemAction from "../../../../Shared/ModalComponents/ModalItemAction";
+import ModalQuantity from "../../../../Shared/ModalComponents/ModalQuantity";
 import { Button } from "react-bootstrap";
 import modalFieldsProvider from "../../../../Shared/ModalComponents/helpers/modalFieldsProvider";
 
@@ -15,22 +15,15 @@ const FermentingIngredientsOrder = () => {
   const { t } = useTranslation();
 
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
-  const [showItemAction, setShowItemAction] = useState(false);
+  const [showQuantityModal, setShowQuantityModal] = useState(false);
   const [modalData, setModalData] = useState([]);
-  const [action, setAction] = useState("default");
+  const [quantityAction, setQuantityAction] = useState("order");
 
   const { ingredientsOrdered } = dbhandler();
 
   const handleDoubleClick = (item) => {
-    setAction("default");
     setModalData({ ...item });
-    setShowItemAction(true);
-  };
-
-  const handleAddOnClick = () => {
-    setAction("add");
-    setModalData(null);
-    setShowItemAction(true);
+    setShowQuantityModal(true);
   };
 
   return (
@@ -41,19 +34,12 @@ const FermentingIngredientsOrder = () => {
         data={ingredientsOrdered}
         handleDoubleClick={handleDoubleClick}
       />
-      <Button variant="dark" onClick={handleAddOnClick}>
-        Add Fermenting Ingredient order
-      </Button>
-      <ModalItemAction
-        fields={modalFieldsProvider(t).fermentingIngredientsModalFields}
-        data={modalData}
-        setData={setModalData}
-        show={showItemAction}
-        setShow={setShowItemAction}
-        setShowConfirmationModal={setShowConfirmationModal}
-        action={action}
-        setAction={setAction}
-        itemName="Fermenting Ingredient"
+      <ModalQuantity
+        fields={modalFieldsProvider(t).quantityModalFields[quantityAction]}
+        modalData={modalData}
+        show={showQuantityModal}
+        setShow={setShowQuantityModal}
+        action={quantityAction}
       />
       <ModalConfirmation
         data={modalData}
