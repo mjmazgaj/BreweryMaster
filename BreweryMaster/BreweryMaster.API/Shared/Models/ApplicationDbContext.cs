8279402852b3dff825fbe602c79f175494ddbc99
Mazgaj <>
2025-01-03 02:05:02 +0100

    Add missing Db Context changes

--- BreweryMaster/BreweryMaster.API/Shared/Models/ApplicationDbContext.cs
+++ BreweryMaster/BreweryMaster.API/Shared/Models/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using BreweryMaster.API.OrderModule.Models;
+﻿using BreweryMaster.API.Info.Models;
+using BreweryMaster.API.Shared.Models;
 using BreweryMaster.API.UserModule.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,13 +14,43 @@ namespace BreweryMaster.API.SharedModule.Models
         {
         }
         public DbSet<Address> Addresses { get; set; }
+        public DbSet<UnitEntity> Units { get; set; }
+
         public DbSet<Order> Orders { get; set; }
         public DbSet<ProspectClient> ProspectClients { get; set; }
         public DbSet<ProspectOrder> ProspectOrders { get; set; }
         public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<FermentingIngredient> FermentingIngredients { get; set; }
+        public DbSet<FermentingIngredientTypeEntity> FermentingIngredientTypes { get; set; }
+        public DbSet<FermentingIngredientUnit> FermentingIngredientUnits { get; set; }
+        public DbSet<FermentingIngredientOrdered> FermentingIngredientOrderedList { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<FermentingIngredientOrdered>(entity =>
+            {
+                entity.HasOne<FermentingIngredientUnit>()
+                      .WithMany()
+                      .HasForeignKey(e => e.FermentingIngredientUnitId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            builder.Entity<FermentingIngredient>(entity =>
+            {
+                entity.HasOne<FermentingIngredientTypeEntity>()
+                      .WithMany()
+                      .HasForeignKey(e => e.TypeId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            builder.Entity<FermentingIngredientTypeEntity>().HasData(FermentingIngredientDataProvider.GetFermentingIngredientTypeEntity());
+            builder.Entity<UnitEntity>().HasData(DataProvider.GetUnitEntity());
+            builder.Entity<FermentingIngredient>().HasData(FermentingIngredientDataProvider.GetFermentingIngredient());
+            builder.Entity<FermentingIngredientUnit>().HasData(FermentingIngredientDataProvider.GetFermentingIngredientUnit());
+            builder.Entity<FermentingIngredientOrdered>().HasData(FermentingIngredientDataProvider.GetFermentingIngredientOrdered());
         }
     }
 }
