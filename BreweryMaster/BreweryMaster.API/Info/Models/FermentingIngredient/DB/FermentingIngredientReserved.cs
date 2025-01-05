677dd0cbf5dc12ccb83abdfbd8f489f5fe63c169
Mazgaj <>
2025-01-05 18:22:32 +0100

    Make OrderId nullable in FermentingIngredientReserved

--- BreweryMaster/BreweryMaster.API/Info/Models/FermentingIngredient/DB/FermentingIngredientReserved.cs
+++ BreweryMaster/BreweryMaster.API/Info/Models/FermentingIngredient/DB/FermentingIngredientReserved.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int FermentingIngredientUnitId { get; set; }
         public float ReservedQuantity { get; set; }
-        public int OrderId { get; set; }
+        public int? OrderId { get; set; }
         public int UserId { get; set; }
         public DateTime ReservationDate { get; set; }
         public bool IsRemoved { get; set; } = false;
