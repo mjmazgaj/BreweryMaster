522b11e0edc294f3ff9d0711673f3df2acec889f
Mazgaj <>
2025-01-14 23:56:55 +0100

    ProspectOrderForm code refactor

--- UI/brewerymaster/src/Components/Order/ProspectOrder/ProspectOrderForm.jsx
+++ UI/brewerymaster/src/Components/Order/ProspectOrder/ProspectOrderForm.jsx
@@ -1,115 +1,20 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { toast } from "react-toastify";
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css';
 import '../order.css';
-import { addData } from '../api';
 
-import ProspectOrderDetails from "./ProspectOrderDetails";
 import MenuSteps from '../../Shared/MenuSteps';
-import ClientDetails from "./ClientDetails";
-import Contact from "../../Shared/Contact"
-import FormDatePicker from "../../Shared/FormDatePicker";
+
+import { useProspectOrderForm } from "./helpers/useProspectOrderForm";
 
 const ProspectOrderForm = () => {  
   
-  const [contactData, setContactData] = useState({
-    phoneNumber: "",
-    email: "",
-  });
-
-  const [prospectOrderData, setProspectOrderData] = useState({
-    selectedContainer: "",
-    selectedBeer: "",
-    capacity: "",
-  });
-
-
-  const [companyClientDetailsData, setCompanyClientDetailsData] = useState({
-    companyName: "",
-    nip: "",
-  });
-  
-  const [individualClientDetailsData, setIndividualClientDetailsData] = useState({
-    forename: "",
-    surname: "",
-  });
-  const [isCompany, setIsCompany] = useState(false);
-
   const [currentStep, setCurrentStep] = useState(0);
-  const [selectedDate, setSelectedDate] = useState(null);
 
-  const handleSave = () => {
-
-    const newData = {
-      forename: individualClientDetailsData.forename,
-      surname: individualClientDetailsData.surname,
-      phoneNumber: contactData.phoneNumber,
-      email: contactData.email,
-      beerStyleId: prospectOrderData.selectedBeer,
-      containerId: prospectOrderData.selectedContainer,
-      capacity: prospectOrderData.capacity,
-      companyName: companyClientDetailsData.companyName,
-      nip: companyClientDetailsData.nip,
-      targetDate: selectedDate,
-      isCompany: isCompany,
-    };
-
-    console.log(newData);
-    addData(newData);
-  };
-
-  const clear = () => {
-    setContactData({
-      forename: "",
-      phoneNumber: "",
-      email: "",
-    });
-  };
-
-  const steps = [
-    {
-      name: "Order",
-      component: (
-        <ProspectOrderDetails
-          setProspectOrderData={setProspectOrderData}
-          prospectOrderData={prospectOrderData}
-        />
-      ),
-    },
-    {
-      name: "ClientDetails",
-      component: (
-        <ClientDetails
-          individualClientDetailsData={individualClientDetailsData}
-          setIndividualClientDetailsData={setIndividualClientDetailsData}
-          companyClientDetailsData={companyClientDetailsData}
-          setCompanyClientDetailsData={setCompanyClientDetailsData}
-          isCompany={isCompany}
-          setIsCompany={setIsCompany}
-        />
-      ),
-    },
-    {
-      name: "Contact",
-      component: (
-        <Contact
-          contactData={contactData}
-          setContactData={setContactData}
-        />
-      ),
-    },
-    {
-      name: "DatePicker",
-      component: (
-        <FormDatePicker
-          selectedDate={selectedDate}
-          setSelectedDate={setSelectedDate}
-        />
-      ),
-    },
-  ];
+  const {steps, handleSave} = useProspectOrderForm();
 
   return (
     <form className="prospectorder-form">
@@ -122,11 +27,11 @@ const ProspectOrderForm = () => {
       <h2>{steps[currentStep].name}</h2>
       <div>{steps[currentStep].component}</div>
 
-      {currentStep === steps.length - 1 ? (
+      {currentStep === steps.length - 1 && (
         <Button variant="dark" onClick={handleSave}>
           Submit
         </Button>
-      ) : null}
+      )}
     </form>
   );
 };
---
+++ UI/brewerymaster/src/Components/Order/ProspectOrder/helpers/useProspectOrderForm.jsx
@@ -0,0 +1,125 @@
+import { React, useState } from "react";
+
+import ProspectOrderDetails from "../ProspectOrderDetails";
+import ClientDetails from "../ClientDetails";
+import Contact from "../../../Shared/Contact"
+import FormDatePicker from "../../../Shared/FormDatePicker";
+
+import { addData } from '../../api';
+
+export const useProspectOrderForm = () => {
+  
+    const [contactData, setContactData] = useState({
+      phoneNumber: "",
+      email: "",
+    });
+    
+      const [prospectOrderData, setProspectOrderData] = useState({
+        selectedContainer: "",
+        selectedBeer: "",
+        capacity: "",
+      });
+    
+      const [companyClientDetailsData, setCompanyClientDetailsData] = useState({
+        companyName: "",
+        nip: "",
+      });
+      
+      const [individualClientDetailsData, setIndividualClientDetailsData] = useState({
+        forename: "",
+        surname: "",
+      });
+      const [isCompany, setIsCompany] = useState(false);
+      const [selectedDate, setSelectedDate] = useState(null);
+    
+    
+      const handleSave = () => {
+    
+        const newData = {
+          forename: individualClientDetailsData.forename,
+          surname: individualClientDetailsData.surname,
+          phoneNumber: contactData.phoneNumber,
+          email: contactData.email,
+          beerStyleId: prospectOrderData.selectedBeer,
+          containerId: prospectOrderData.selectedContainer,
+          capacity: prospectOrderData.capacity,
+          companyName: companyClientDetailsData.companyName,
+          nip: companyClientDetailsData.nip,
+          targetDate: selectedDate,
+          isCompany: isCompany,
+        };
+    
+        addData(newData);
+        clear();
+      };
+    
+      const clear = () => {
+        setContactData({
+          phoneNumber: "",
+          email: "",
+        });
+        setProspectOrderData ({
+          selectedContainer: "",
+          selectedBeer: "",
+          capacity: "",
+        });
+        setCompanyClientDetailsData ({
+          companyName: "",
+          nip: "",
+        });
+        setIndividualClientDetailsData({
+          forename: "",
+          surname: "",
+        });
+        setIsCompany(false);
+        setSelectedDate(null);
+      };
+    
+      const steps = [
+        {
+          name: "Order",
+          component: (
+            <ProspectOrderDetails
+              setProspectOrderData={setProspectOrderData}
+              prospectOrderData={prospectOrderData}
+            />
+          ),
+        },
+        {
+          name: "ClientDetails",
+          component: (
+            <ClientDetails
+              individualClientDetailsData={individualClientDetailsData}
+              setIndividualClientDetailsData={setIndividualClientDetailsData}
+              companyClientDetailsData={companyClientDetailsData}
+              setCompanyClientDetailsData={setCompanyClientDetailsData}
+              isCompany={isCompany}
+              setIsCompany={setIsCompany}
+            />
+          ),
+        },
+        {
+          name: "Contact",
+          component: (
+            <Contact
+              contactData={contactData}
+              setContactData={setContactData}
+            />
+          ),
+        },
+        {
+          name: "Complition date",
+          component: (
+            <FormDatePicker
+              selectedDate={selectedDate}
+              setSelectedDate={setSelectedDate}
+            />
+          ),
+        },
+      ];
+
+  return {
+    steps,
+    handleSave
+  };
+};
