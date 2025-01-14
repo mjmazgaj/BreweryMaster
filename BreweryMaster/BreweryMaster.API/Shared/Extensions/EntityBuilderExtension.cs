b9cafd5de5c5e3aa483a86b1520daf04762d7909
Mazgaj <>
2025-01-14 01:02:45 +0100

    add missing file

---
+++ BreweryMaster/BreweryMaster.API/Shared/Extensions/EntityBuilderExtension.cs
@@ -0,0 +1,21 @@
+﻿using BreweryMaster.API.Shared.Models.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace BreweryMaster.API.Shared.Extensions
+{
+    public static class EntityBuilderExtension
+    {
+        public static void ConfigureEntity(this ModelBuilder builder)
+        {
+            builder.Entity<Container>(entity =>
+                entity.HasOne(e => e.UnitEntity)
+                      .WithMany()
+                      .HasForeignKey(e => e.UnitEntityId)
+                      .OnDelete(DeleteBehavior.Cascade)
+            );
+
+            builder.Entity<UnitEntity>().HasData(EntityDataProvider.GetUnitEntity());
+            builder.Entity<Container>().HasData(EntityDataProvider.GetContainers());
+        }
+    }
+}
