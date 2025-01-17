30dabe335a203d327fd175edd95505c1594988a2
Mazgaj <>
2025-01-17 01:05:52 +0100

    Create generic component ModalFormBasic

---
+++ UI/brewerymaster/src/Components/Shared/ModalComponents/ModalFormBasic.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
+import { useModalFormBasic } from "./helpers/useModalFormBasic";
+import FormControls from "../FormControls";
+
+import { useTranslation } from "react-i18next";
+import DropDownIndex from "../DropDownIndex";
+
+const ModalFormBasic = ({
+  fields,
+  data,
+  setData,
+  show,
+  setShow,
+  action,
+  itemName,
+}) => {
+  const { t } = useTranslation();
+  const [isValid, setIsValid] = useState(true);
+
+  const { handleClose, actionObject, handleCheckBox, handleSelectChange } = useModalFormBasic({
+    data,
+    setData,
+    setShow,
+    action,
+    itemName,
+    isValid,
+  });
+
+  const renderDropdowns = () =>
+    fields.dropdown &&
+    fields.dropdown.map((dropdownObject, index) => (
+      <DropDownIndex
+        key={index}
+        id={"modal-form_dropdown"}
+        data={dropdownObject.data}
+        selectedOption={data[dropdownObject.name]}
+        setSelectedOption={(e) => handleSelectChange(e, dropdownObject.name)}
+        isReadOnly={actionObject.isReadOnly}
+        label={dropdownObject.label}
+      />
+    ));
+
+  const renderCheckBoxes = () =>(
+    action == "add" && fields.checkBox && (
+      <div className="modal-form_checkbox-container">
+        <h5>{t("common.selectUnits")}</h5>
+        {fields.checkBox.map((checkBoxObject) => (
+          <Form.Check
+            type="switch"
+            key={checkBoxObject.id}
+            id={checkBoxObject.id}
+            label={checkBoxObject.name}
+            checked={data[checkBoxObject.name]}
+            onChange={() => handleCheckBox(checkBoxObject)}
+          />
+        ))}
+      </div>
+    )
+  )
+  
+  return (
+    fields && (
+      <Modal show={show} onHide={handleClose}>
+        <Form onSubmit={(event) => actionObject.function(event, data)}>
+          <Modal.Header closeButton>
+            <Modal.Title>{actionObject.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {renderDropdowns()}
+            <FormControls
+              fields={fields.control}
+              data={data}
+              setData={setData}
+              isReadOnly={actionObject.isReadOnly}
+              setIsValid={setIsValid}
+            />
+            {renderCheckBoxes()}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="submit" variant="dark" disabled={!isValid}>
+              {action == "add" ? t("button.add") : t("button.saveChanges")}
+            </Button>
+            <Button variant="dark" onClick={handleClose}>
+              {t("button.close")}
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    )
+  );
+};
+
+export default ModalFormBasic;
---
+++ UI/brewerymaster/src/Components/Shared/ModalComponents/helpers/useModalFormBasic.jsx
@@ -0,0 +1,86 @@
+import { useTranslation } from "react-i18next";
+
+export const useModalFormBasic = ({
+  data,
+  setData,
+  setShow,
+  action,
+  itemName,
+  isValid
+}) => {
+  const { t } = useTranslation();
+
+  const handleClose = () => {
+    setShow(false);
+  };
+  
+  const handleCheckBox = (item) => {
+    console.log(item)
+    console.log(data)
+    setData((prevData) =>({
+      ...prevData,
+      [item.name]: !prevData[item.name]
+    }))
+  };
+
+  const handleFormSubmit = (event) =>{
+    const form = event.currentTarget;
+    event.preventDefault();
+
+    if (form.checkValidity() === false || !isValid) {
+      event.stopPropagation();
+      return false;
+    }
+
+    return true;
+  }
+  
+  const handleSelectChange = (e, name) => {
+    const { value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleAdd = (event, data) => {
+    if (!handleFormSubmit(event)) {
+      return;
+    }
+    console.log("add");
+    console.log({...data});
+    setShow(false);
+  };
+
+  const handleEdit = (event, data) => {
+    if (!handleFormSubmit(event)) {
+      return;
+    }
+    
+    console.log("edit");
+    console.log({...data});
+    setShow(false);
+  };
+
+  const actionCases = {
+    add: {
+      title: `Add ${itemName}`,
+      function: handleAdd,
+      isReadOnly: false,
+    },
+    edit: {
+      title: `Edit ${data ? data.name : ""}`,
+      function: handleEdit,
+      isReadOnly: false,
+    },
+  };
+
+  let actionObject = actionCases[action];
+
+  return {
+    handleClose,
+    actionObject,
+    handleCheckBox,
+    handleSelectChange
+  };
+};
