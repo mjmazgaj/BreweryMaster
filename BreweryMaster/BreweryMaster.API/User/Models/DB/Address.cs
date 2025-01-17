85948d66f02debcfb71ab4376092025bbfe672ba
Mazgaj <>
2025-01-17 23:47:10 +0100

    Update Address model

---
+++ BreweryMaster/BreweryMaster.API/User/Models/DB/Address.cs
@@ -0,0 +1,20 @@
+﻿using BreweryMaster.API.User.Models.Users.DB;
+
+namespace BreweryMaster.API.UserModule.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string? Street { get; set; }
+        public required string HouseNumber { get; set; }
+        public string? ApartamentNumber { get; set; }
+        public required string PostalCode { get; set; }
+        public required string City { get; set; }
+        public required string Commune { get; set; }
+        public required string Region { get; set; }
+        public required string Country { get; set; }
+
+        public required string UserId { get; set; }
+        public required ApplicationUser User { get; set; }
+    }
+}
