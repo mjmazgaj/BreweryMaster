b7e7c133f7d21778d1a49fe315fc2dc93b3f7bab
Mazgaj <>
2025-01-24 00:58:17 +0100

    add User provider

---
+++ UI/brewerymaster/src/Components/Basic/UserProvider.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState(() => {
+    const savedUser = sessionStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : { 
+      role: []
+    };
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
