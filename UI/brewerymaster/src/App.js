c4490676ad5fa462d123045e3bf2b4036dd00243
Mazgaj <>
2025-01-26 16:51:30 +0100

    create useApp

--- UI/brewerymaster/src/App.js
+++ UI/brewerymaster/src/App.js
@@ -1,37 +1,20 @@
-import Home from './Components/App/Home';
 import Navigation from './Components/App/Navigation';
 
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import './Components/App/App.css';
-import React, { useState, useEffect } from 'react';
-
-import Register from './Components/Security/Register';
-import Login from './Components/Security/Login';
-import Error from './Components/Shared/Error';
-
-import Client from './Components/User/Client/Client';
-import User from './Components/User/User.jsx';
-
-import Order from './Components/Order/Order';
-import Recipe from './Components/Recipe/Recipe';
-import FermentingIngredients from './Components/Info/FermentingIngredients/FermentingIngredients';
-
-import Kanban from './Components/Work/Kanban';
+import React from 'react';
 
 import ProtectedRoute from './Components/Security/ProtectedRoute'
 
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-import ProspectOrderForm from './Components/Order/ProspectOrderForm.jsx';
-import ProspectOrderSummary from './Components/Order/ProspectOrderSummary.jsx';
-import Unauthorized from './Components/App/Unuthorized';
-
-import { useUser } from './Components/Security/UserProvider';
 import Footer from './Components/App/Footer.jsx';
+import { useApp } from './Components/App/helpers/useApp.jsx';
 const App = () => {
-  const { user } = useUser();
-  
+
+  const {routes, protectedRoutes} = useApp();
+
   return (
     <>
       <I18nextProvider i18n={i18n}>
@@ -39,90 +22,26 @@ const App = () => {
         <Router>
           <div className="App">
             <Routes>
-              <Route exact path="/" element={<Home />} />
               <Route path="*" element={<Navigate to="/" />} />
 
-              <Route path="/register" element={<Register />} />
-              <Route
-                path="/login"
-                element={<Login />}
-              />
+              {routes.map(({ path, element }, index) => (
+                <Route key={index} exact path={path} element={element} />
+              ))}
 
-              <Route
-                exact
-                path="/ProspectOrder"
-                element={<ProspectOrderForm />}
-              />
-              <Route
-                exact
-                path="/ProspectOrderSummary"
-                element={<ProspectOrderSummary />}
-              />
-
-              <Route
-                exact
-                path="/Order"
-                element={
-                  <ProtectedRoute roles={["supervisor"]}>
-                    <Order />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                exact
-                path="/Recipe"
-                element={
-                  <ProtectedRoute roles={["brewer"]}>
-                    <Recipe />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route
-                exact
-                path="/FermentingIngredients"
-                element={
-                  <ProtectedRoute roles={["supervisor"]}>
-                    <FermentingIngredients />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route
-                exact
-                path="/Client"
-                element={
-                  <ProtectedRoute roles={["manager"]}>
-                    <Client />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                exact
-                path="/User"
-                element={
-                  <ProtectedRoute roles={["manager"]}>
-                    <User />
-                  </ProtectedRoute>
-                }
-              />
-
-              <Route exact path="/Error" element={<Error />} />
-              <Route exact path="/Unauthorized" element={<Unauthorized />} />
-
-              <Route
-                path="/Kanban"
-                element={
-                  <ProtectedRoute roles={["employee"]}>
-                    <Kanban />
-                  </ProtectedRoute>
-                }
-              ></Route>
+              {protectedRoutes.map(({ path, roles, element }, index) => (
+                <Route
+                  key={index}
+                  exact
+                  path={path}
+                  element={
+                    <ProtectedRoute roles={roles}>{element}</ProtectedRoute>
+                  }
+                />
+              ))}
             </Routes>
-
           </div>
         </Router>
-        <Footer/>
+        <Footer />
       </I18nextProvider>
     </>
   );
---
+++ UI/brewerymaster/src/Components/App/helpers/useApp.jsx
@@ -0,0 +1,42 @@
+
+import Home from '../Home';
+import Register from '../../Security/Register';
+import Login from '../../Security/Login';
+import ProspectOrderForm from '../../Order/ProspectOrderForm';
+import ProspectOrderSummary from '../../Order/ProspectOrderSummary';
+import Order from '../../Order/Order';
+import Recipe from '../../Recipe/Recipe';
+import Client from '../../User/Client/Client';
+import FermentingIngredients from '../../Info/FermentingIngredients/FermentingIngredients';
+import User from '../../User/User';
+import Kanban from '../../Work/Kanban';
+import Unauthorized from '../Unuthorized';
+import Error from '../../Shared/Error';
+
+
+export const useApp = () => {
+    const routes = [
+      { path: "/", element: <Home /> },
+      { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> },
+      { path: "/ProspectOrder", element: <ProspectOrderForm /> },
+      { path: "/ProspectOrderSummary", element: <ProspectOrderSummary /> },
+      { path: "/Error", element: <Error /> },
+      { path: "/Unauthorized", element: <Unauthorized /> },
+    ];
+
+    const protectedRoutes = [
+      { path: "/Order", roles:["supervisor"], element: <Order /> },
+      { path: "/Recipe", roles:["brewer"], element: <Recipe /> },
+      { path: "/FermentingIngredients", roles:["supervisor"], element: <FermentingIngredients /> },
+      { path: "/Client", roles:["manager"], element: <Client /> },
+      { path: "/User", roles:["manager"], element: <User /> },
+      { path: "/Kanban", roles:["employee"], element: <Kanban /> },
+    ];
+
+    return {
+      routes,
+      protectedRoutes
+    };
+  };
+  
