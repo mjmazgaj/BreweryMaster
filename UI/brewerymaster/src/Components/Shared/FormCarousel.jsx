90c8571efc113bb58c94946339e071eea7b5ce3b
Mazgaj <>
2025-01-27 00:53:52 +0100

    Add FormCarousel

---
+++ UI/brewerymaster/src/Components/Shared/FormCarousel.jsx
@@ -0,0 +1,33 @@
+import { React, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./shared.css"
+
+import MenuSteps from './MenuSteps';
+
+const FormCarousel = ({steps, handleSave}) => {
+
+  const [currentStep, setCurrentStep] = useState(0);
+
+  return (
+    <Form className="form-carousel_container">
+      <MenuSteps
+        currentStep={currentStep}
+        setCurrentStep={setCurrentStep}
+        amountOfSteps={steps.length}
+      />
+
+      <h2>{steps[currentStep].name}</h2>
+      <div>{steps[currentStep].component}</div>
+
+      {currentStep === steps.length - 1 && (
+        <Button variant="dark" onClick={handleSave}>
+          Submit
+        </Button>
+      )}
+    </Form>
+  );
+};
+
+export default FormCarousel;
