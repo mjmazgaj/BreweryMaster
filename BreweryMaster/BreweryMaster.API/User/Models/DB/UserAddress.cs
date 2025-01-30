defb8eb76ac361063b678538b589f18655e0d6a8
Mazgaj <>
2025-01-30 01:28:23 +0100

    Add missing comments for UserAddress

--- BreweryMaster/BreweryMaster.API/User/Models/DB/UserAddress.cs
+++ BreweryMaster/BreweryMaster.API/User/Models/DB/UserAddress.cs
@@ -9,21 +9,33 @@ namespace BreweryMaster.API.User.Models.DB
     public class UserAddress
     {
         /// <summary>
-        /// The user in relation. 
+        /// The user id in relation. 
         /// </summary>
         public required string UserId { get; set; }
+
+        /// <summary>
+        /// The user model representation
+        /// </summary>
         public required ApplicationUser User { get; set; }
 
         /// <summary>
         /// The address in relation. 
         /// </summary>
         public int AddressId { get; set; }
+
+        /// <summary>
+        /// The address model representation
+        /// </summary>
         public required Address Address { get; set; }
 
         /// <summary>
         /// The address type in relation. 
         /// </summary>
         public int AddressTypeId { get; set; }
+
+        /// <summary>
+        /// The address type model representation
+        /// </summary>
         public required AddressTypeEntity AddressType { get; set; }
     }
 }
