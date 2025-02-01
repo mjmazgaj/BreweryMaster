a8fa75ea4c3506d08b2c19192d7a155b4d73f475
Mazgaj <>
2025-02-01 14:47:08 +0100

    Wokr : Add proper attributes

--- BreweryMaster/BreweryMaster.API/Work/Models/DB/KanbanTask.cs
+++ BreweryMaster/BreweryMaster.API/Work/Models/DB/KanbanTask.cs
@@ -1,22 +1,31 @@
 ﻿using BreweryMaster.API.OrderModule.Models;
 using BreweryMaster.API.User.Models.Users.DB;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace BreweryMaster.API.Work.Models.DB
 {
     public class KanbanTask
     {
         public int Id { get; set; }
+        [MaxLength(255)]
         public required string Title { get; set; }
         public string? Summary { get; set; }
-        public required TaskStatusEntity Status { get; set; }
         public int StatusId { get; set; }
+        [JsonIgnore]
+        public required TaskStatusEntity Status { get; set; }
         public DateTime DueDate { get; set; }
+        [MaxLength(450)]
         public required string CreatedById { get; set; }
+        [JsonIgnore]
         public required ApplicationUser CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
+        [MaxLength(450)]
         public string? AssignedToId { get; set; }
+        [JsonIgnore]
         public ApplicationUser? AssignedTo { get; set; }
         public int? OrderId { get; set; }
+        [JsonIgnore]
         public Order? Order { get; set; }
     }
 }
--- BreweryMaster/BreweryMaster.API/Work/Models/DB/TaskStatusEntity.cs
+++ BreweryMaster/BreweryMaster.API/Work/Models/DB/TaskStatusEntity.cs
@@ -1,8 +1,11 @@
-﻿namespace BreweryMaster.API.Work.Models.DB
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BreweryMaster.API.Work.Models.DB
 {
     public class TaskStatusEntity
     {
         public int Id { get; set; }
+        [MaxLength(255)]
         public required string Name { get; set; }
     }
 }
