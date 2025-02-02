e126b212ebccaa8a6eada7e7ccb190feaa949226
Mazgaj <>
2025-02-02 15:49:11 +0100

    Log : Add DB log model

---
+++ BreweryMaster/BreweryMaster.API/Log/Models/DB/ChangeLog.cs
@@ -0,0 +1,11 @@
+﻿namespace BreweryMaster.API.Log.Models.DB
+{
+    public class ChangeLog
+    {
+        public int Id { get; set; }
+        public required string TableName { get; set; }
+        public required string ChangeType { get; set; }
+        public required string ChangedData { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ BreweryMaster/BreweryMaster.API/Shared/Models/DB/DBContextLog.cs
@@ -0,0 +1,10 @@
+﻿using BreweryMaster.API.Log.Models.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace BreweryMaster.API.Shared.Models.DB
+{
+    public partial class ApplicationDbContext
+    {
+        public DbSet<ChangeLog> ChangeLogs { get; set; }
+    }
+}
