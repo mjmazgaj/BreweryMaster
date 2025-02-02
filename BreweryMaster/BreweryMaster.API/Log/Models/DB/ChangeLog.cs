375043c291fa2d32b5ea1bad50c5fd93244cfc3d
Mazgaj <>
2025-02-02 15:51:49 +0100

    Log : Add Max Length attributes

--- BreweryMaster/BreweryMaster.API/Log/Models/DB/ChangeLog.cs
+++ BreweryMaster/BreweryMaster.API/Log/Models/DB/ChangeLog.cs
@@ -3,7 +3,9 @@ namespace BreweryMaster.API.Log.Models
     public class ChangeLog
     {
         public int Id { get; set; }
+        [MaxLength(255)]
         public required string TableName { get; set; }
+        [MaxLength(255)]
         public required string ChangeType { get; set; }
         public required string ChangedData { get; set; }
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
