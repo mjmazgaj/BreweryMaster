88fb400de2256acb07d192cadd73054e89b4f739
Mazgaj <>
2025-02-19 21:30:15 +0100

    Client | Remove redundant components

--- UI/brewerymaster/src/Components/App/helpers/useApp.jsx
+++ UI/brewerymaster/src/Components/App/helpers/useApp.jsx
@@ -7,7 +7,6 @@ import ProspectOrderSummary from '../../Order/ProspectOrderSummary';
 import Order from '../../Order/Order';
 import Recipe from '../../Recipe/Recipe';
 import RecipeDetails from '../../Recipe/RecipeDetails';
-import Client from '../../User/Client/Client';
 import FermentingIngredients from '../../Info/FermentingIngredients/FermentingIngredients';
 import User from '../../User/User';
 import UserDetails from '../../User/UserDetails';
@@ -34,7 +33,6 @@ export const useApp = () => {
       { path: "/Recipe", roles:["brewer"], element: <Recipe /> },
       { path: "/Recipe/:id", roles:["brewer"], element: <RecipeDetails /> },
       { path: "/FermentingIngredients", roles:["supervisor"], element: <FermentingIngredients /> },
-      { path: "/Client", roles:["manager"], element: <Client /> },
       { path: "/User", roles:["manager"], element: <User /> },
       { path: "/User/:id", roles:["manager"], element: <UserDetails /> },
       { path: "/Kanban", roles:["employee"], element: <Kanban /> },
