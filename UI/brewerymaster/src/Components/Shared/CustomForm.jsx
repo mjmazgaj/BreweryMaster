499850af5b6091d42730ea234c6a3d262bf3a71c
Mazgaj <>
2025-02-22 19:09:39 +0100

    Form : Add CustomForm

---
+++ UI/brewerymaster/src/Components/Shared/CustomForm.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+import { useCustomForm } from "./ModalComponents/helpers/useCustomForm";
+import FormControls from "./FormControls";
+
+import DropDownIndex from "./DropDownIndex";
+import FormDatePicker from "./FormDatePicker";
+
+const CustomForm = ({
+  fields,
+  formCustomizationObject,
+}) => {
+
+  const [data, setData] = useState({});
+  const [isValid, setIsValid] = useState({});
+
+  const {
+    handleCheckBox,
+    handleSelectChange,
+    handleDateChange,
+    handleFormSubmit,
+  } = useCustomForm({
+    setData,
+    isValid,
+    formCustomizationObject,
+  });
+
+  const renderDropdowns = () =>
+    fields?.dropdown && (
+      <div
+        className={`${formCustomizationObject.classNamePrefix}_dropdown-container`}
+      >
+        <hr />
+        {fields.dropdown.map((dropdownObject, index) => (
+          <DropDownIndex
+            key={`${index}_dropdown`}
+            id={`${formCustomizationObject.classNamePrefix}_dropdown`}
+            data={dropdownObject.data}
+            selectedOption={data[dropdownObject.name]}
+            setSelectedOption={(e) =>
+              handleSelectChange(e, dropdownObject.name)
+            }
+            label={dropdownObject.label}
+          />
+        ))}
+      </div>
+    );
+
+  const renderCheckBoxes = () =>
+    fields?.checkBox && (
+      <div
+        className={`${formCustomizationObject.classNamePrefix}_checkbox-container`}
+      >
+        <hr />
+        {fields.checkBox.map((checkBoxObject, index) => (
+          <Form.Check
+            type="switch"
+            key={`${index}_checkbox`}
+            id={checkBoxObject.name}
+            label={checkBoxObject.label}
+            onChange={(e) =>
+              handleCheckBox(checkBoxObject.id, e.target.checked)
+            }
+          />
+        ))}
+      </div>
+    );
+
+  const renderDatePickers = () =>
+    fields?.datePicker && (
+      <div
+        className={`${formCustomizationObject.classNamePrefix}_datepicker-container`}
+      >
+        <hr />
+        {fields.datePicker.map((datePickerObject, index) => (
+          <FormDatePicker
+            key={`datepicker-${index}`}
+            id={`datepicker-${datePickerObject.name}`}
+            label={datePickerObject.label}
+            selectedDate={data[datePickerObject.name]}
+            setSelectedDate={(date) =>
+              handleDateChange(date, datePickerObject.name)
+            }
+          />
+        ))}
+      </div>
+    );
+
+  return (
+    fields && (
+      <Form onSubmit={(event) => handleFormSubmit(event, data)}>
+        <h2>{formCustomizationObject?.title ?? ""}</h2>
+        <FormControls
+          fields={fields.control}
+          data={data}
+          setData={setData}
+          setIsValid={setIsValid}
+        />
+        {renderDropdowns()}
+        {renderCheckBoxes()}
+        {renderDatePickers()}
+        {formCustomizationObject?.buttons &&
+          formCustomizationObject.buttons.map((button, index) => (
+            <Button
+              type={button.isSubmit ? "submit" : ""}
+              key={index}
+              variant="dark"
+              disabled={!isValid}
+              onClick={button?.function ?? null}
+            >
+              {button.label}
+            </Button>
+          ))}
+      </Form>
+    )
+  );
+};
+
+export default CustomForm;
---
+++ UI/brewerymaster/src/Components/Shared/ModalComponents/helpers/useCustomForm.jsx
@@ -0,0 +1,54 @@
+export const useCustomForm = ({
+  setData,
+  isValid,
+  formCustomizationObject,
+}) => {
+  const handleCheckBox = (unitId, isChecked) => {
+    setData((prevData) => ({
+      ...prevData,
+      units: isChecked
+        ? [...prevData.units, unitId]
+        : prevData.units.filter((x) => x !== unitId),
+    }));
+  };
+
+  const handleSelectChange = (e, name) => {
+    const { value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: parseInt(value),
+    }));
+  };
+
+  const handleDateChange = (date, fieldName) => {
+    setData((prevData) => ({
+      ...prevData,
+      [fieldName]: date,
+    }));
+  };
+
+  const handleFormSubmit = (event, data) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+
+    if (form.checkValidity() === false || !isValid) {
+      event.stopPropagation();
+      return;
+    }
+
+    if (
+      formCustomizationObject?.addtionalValidation &&
+      !formCustomizationObject.addtionalValidation(data)
+    )
+      return;
+      
+      formCustomizationObject.submitFunction(data);
+  };
+
+  return {
+    handleCheckBox,
+    handleSelectChange,
+    handleDateChange,
+    handleFormSubmit,
+  };
+};
