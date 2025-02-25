e6e0f66a46fc33e844034ba823e0fff3b9286ac2
Mazgaj <>
2025-02-25 14:11:17 +0100

    Work | Controller : Apply roles and responses approach

--- BreweryMaster/BreweryMaster.API/Work/Controllers/TaskController.cs
+++ BreweryMaster/BreweryMaster.API/Work/Controllers/TaskController.cs
@@ -1,7 +1,9 @@
-﻿using BreweryMaster.API.Work.Models;
+﻿using BreweryMaster.API.SharedModule.Validators;
+using BreweryMaster.API.Work.Models;
 using BreweryMaster.API.Work.Models.Requests;
 using BreweryMaster.API.WorkModule.Models;
 using BreweryMaster.API.WorkModule.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BreweryMaster.API.WorkModule.Controllers
@@ -20,7 +22,10 @@ namespace BreweryMaster.API.WorkModule.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "employee")]
         [ProducesResponseType(typeof(Dictionary<string, KanbanTaskGroupResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<ActionResult<Dictionary<string, KanbanTaskGroupResponse>>?> GetKanbanTasksByOwnerId([FromQuery] KanbanTaskFilterRequest? request)
         {
             var tasks = await _taskService.GetKanbanTasksByOwnerIdAsync(request);
@@ -29,9 +34,12 @@ namespace BreweryMaster.API.WorkModule.Controllers
 
         [HttpGet]
         [Route("{id:int}")]
+        [Authorize(Roles = "employee")]
         [ProducesResponseType(typeof(KanbanTaskResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<KanbanTaskResponse>> GetKanbanTaskById(int id)
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<KanbanTaskResponse>> GetKanbanTaskById([MinIntValidation] int id)
         {
             var task = await _taskService.GetKanbanTaskByIdAsync(id);
 
@@ -42,11 +50,19 @@ namespace BreweryMaster.API.WorkModule.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles = "brewer,supervisor")]
         [ProducesResponseType(typeof(KanbanTaskResponse), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<ActionResult<KanbanTaskResponse>> CreateKanbanTask([FromBody] KanbanTaskRequest kanbanTask)
         {
-            var createdTask = await _taskService.CreateKanbanTaskAsync(kanbanTask, HttpContext.User);
+            var userIdentity = HttpContext?.User?.Identity;
+
+            if (userIdentity is null || !userIdentity.IsAuthenticated)
+                return Unauthorized();
+
+            var createdTask = await _taskService.CreateKanbanTaskAsync(kanbanTask, HttpContext?.User);
 
             if (createdTask == null)
                 return Unauthorized();
@@ -56,11 +72,19 @@ namespace BreweryMaster.API.WorkModule.Controllers
 
         [HttpPost]
         [Route("Template")]
+        [Authorize(Roles = "brewer,supervisor")]
         [ProducesResponseType(typeof(IEnumerable<KanbanTaskResponse>), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<ActionResult<IEnumerable<KanbanTaskResponse>>> CreateKanbanTaskTemplates(KanbanTaskTemplateRequest request)
         {
-            var createdTasks = await _taskService.CreateKanbanTaskTemplates(request, HttpContext.User);
+            var userIdentity = HttpContext?.User?.Identity;
+
+            if (userIdentity is null || !userIdentity.IsAuthenticated)
+                return Unauthorized();
+
+            var createdTasks = await _taskService.CreateKanbanTaskTemplates(request, HttpContext?.User);
 
             if (createdTasks == null)
                 return Unauthorized();
@@ -70,43 +94,51 @@ namespace BreweryMaster.API.WorkModule.Controllers
 
         [HttpPatch]
         [Route("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> EditKanbanTask(int id, [FromBody] KanbanTaskUpdateRequest kanbanTask)
+        [Authorize(Roles = "brewer,supervisor")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> EditKanbanTask([MinIntValidation] int id, [FromBody] KanbanTaskUpdateRequest kanbanTask)
         {
             var isUpdated = await _taskService.EditKanbanTaskAsync(id, kanbanTask);
 
             if (!isUpdated)
-                return BadRequest();
+                return NotFound();
 
             return Ok(isUpdated);
         }
 
         [HttpPatch]
         [Route("EditStatus")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Authorize(Roles = "employee")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status404NotFound)]
         public async Task<ActionResult> EditKanbanTaskStatus([FromBody] List<KanbanTaskStatusRequest> request)
         {
             var isUpdated = await _taskService.EditKanbanTaskStatusAsync(request);
 
             if (!isUpdated)
-                return BadRequest();
+                return NotFound();
 
             return Ok(isUpdated);
         }
 
         [HttpPatch]
         [Route("Delete/{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> DeleteKanbanTaskById(int id)
+        [Authorize(Roles = "brewer,supervisor")]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteKanbanTaskById([MinIntValidation] int id)
         {
             var isDeleted = await _taskService.DeleteKanbanTaskByIdAsync(id);
 
             if (!isDeleted)
-                return BadRequest();
+                return NotFound();
 
             return Ok(isDeleted);
         }
