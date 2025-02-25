c1f52322385c49351ef98f6275da2c20a24116af
Mazgaj <>
2025-02-25 23:31:13 +0100

    BaseTestController refactor

--- BreweryMaster/BreweryMaster.Tests/Controllers/BaseTestController.cs
+++ BreweryMaster/BreweryMaster.Tests/Controllers/BaseTestController.cs
@@ -1,17 +1,11 @@
 ﻿using BreweryMaster.API.WorkModule.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using System.Net.Http.Headers;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System.Text.Encodings.Web;
 
 public class BaseTestController : IDisposable
 {
@@ -52,52 +46,5 @@ public class BaseTestController : IDisposable
     public void Dispose()
     {
         Client.Dispose();
-    }
-}
-
-public class ValidateModelFilter : IActionFilter
-{
-    public void OnActionExecuting(ActionExecutingContext context)
-    {
-        if (!context.ModelState.IsValid)
-        {
-            context.Result = new BadRequestObjectResult(context.ModelState);
-        }
-    }
-
-    public void OnActionExecuted(ActionExecutedContext context) { }
-}
-
-public class TestAuthOptions : AuthenticationSchemeOptions { }
-
-public class TestAuthHandler : AuthenticationHandler<TestAuthOptions>
-{
-    public TestAuthHandler(
-        IOptionsMonitor<TestAuthOptions> options,
-        ILoggerFactory logger,
-        UrlEncoder encoder)
-        : base(options, logger, encoder)
-    { }
-
-    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
-    {
-        var claims = new[]
-        {
-            new Claim(ClaimTypes.NameIdentifier, "123"),
-            new Claim(ClaimTypes.Email, "test@test.test"),
-            new Claim(ClaimTypes.Name, "Test User"),
-            new Claim(ClaimTypes.Role, "brewer"),
-            new Claim(ClaimTypes.Role, "customer"),
-            new Claim(ClaimTypes.Role, "employee"),
-            new Claim(ClaimTypes.Role, "employeeNotMobile"),
-            new Claim(ClaimTypes.Role, "manager"),
-            new Claim(ClaimTypes.Role, "supervisor")
-        };
-
-        var identity = new ClaimsIdentity(claims, "TestAuth");
-        var principal = new ClaimsPrincipal(identity);
-        var ticket = new AuthenticationTicket(principal, "TestAuth");
-
-        return Task.FromResult(AuthenticateResult.Success(ticket));
     }
 }
---
+++ BreweryMaster/BreweryMaster.Tests/Models/TestAuthHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+
+public class TestAuthHandler : AuthenticationHandler<TestAuthOptions>
+{
+    public TestAuthHandler(
+        IOptionsMonitor<TestAuthOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder)
+        : base(options, logger, encoder)
+    { }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, "123"),
+            new Claim(ClaimTypes.Email, "test@test.test"),
+            new Claim(ClaimTypes.Name, "Test User"),
+            new Claim(ClaimTypes.Role, "brewer"),
+            new Claim(ClaimTypes.Role, "customer"),
+            new Claim(ClaimTypes.Role, "employee"),
+            new Claim(ClaimTypes.Role, "employeeNotMobile"),
+            new Claim(ClaimTypes.Role, "manager"),
+            new Claim(ClaimTypes.Role, "supervisor")
+        };
+
+        var identity = new ClaimsIdentity(claims, "TestAuth");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "TestAuth");
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
