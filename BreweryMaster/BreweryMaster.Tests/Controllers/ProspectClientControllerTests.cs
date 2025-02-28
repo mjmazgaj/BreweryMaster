d0c951973967b21eed69bcbf37e3d7c1d939aa9d
Mazgaj <>
2025-02-28 23:36:49 +0100

    ProspectClient | Add tests

--- BreweryMaster/BreweryMaster.Tests/Controllers/ProspectClientControllerTests.cs
+++ BreweryMaster/BreweryMaster.Tests/Controllers/ProspectClientControllerTests.cs
@@ -1,6 +1,27 @@
-﻿namespace BreweryMaster.Tests.Controllers
+﻿using BreweryMaster.API.OrderModule.Models;
+using BreweryMaster.Tests.Models;
+using Moq;
+using System.Net;
+
+namespace BreweryMaster.Tests.Controllers
 {
     public class ProspectClientControllerTests : BaseTestController
     {
+
+        [Fact]
+        public async Task GetProspectClients_ShouldReturnProperResponse()
+        {
+            // Arrange
+            var response = new List<ProspectClientResponse>();
+
+            MockProspectClientService.Setup(s => s.GetProspectClientsAsync())
+                .ReturnsAsync(response);
+
+            // Act
+            var httpResponse = await Client.GetAsync(EndpointsConst.ProspectClient);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
     }
 }
